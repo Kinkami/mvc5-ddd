fc1b8186b7ef057a2a15c5cf66e5cedac35f6683
Fernando <fernando.henrique.leme@gmail.com>
2019-02-24 22:37:18 -0300

    01:35 de vídeo Mapper
    instalado o mapper mas devo reverter para a versao que utiliza no video pois mudatam alguns metodos

---
+++ ProjetoModeloDDD/ProjetoModeloDDD.Infra.Data/Repositories/ProdutoRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ProjetoModeloDDD.Domain.Entities;
+using ProjetoModeloDDD.Domain.Interfaces;
+
+namespace ProjetoModeloDDD.Infra.Data.Repositories
+{
+    class ProdutoRepository : RepositoryBase<Produto>, IprodutoRepository
+    {
+        public IEnumerable<Produto> BuscarPorNome(string nome)
+        {
+           return Db.Produtos.Where(prop => p.Nome == nome);
+        }
+    }
+}
